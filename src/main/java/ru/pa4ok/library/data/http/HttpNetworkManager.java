4cd5a6c123b02ce3fbd92da17b068afafc79fae5
Pa4ok <n_leontiev@mail.ru>
2020-08-30 23:44:00 +0300

    update HttpNetworkManager, add custom headers

--- src/main/java/ru/pa4ok/library/data/http/HttpNetworkManager.java
+++ src/main/java/ru/pa4ok/library/data/http/HttpNetworkManager.java
@@ -8,9 +8,12 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import ru.pa4ok.library.data.message.JsonServerMessage;
 import ru.pa4ok.library.util.GsonUtil;
+import ru.pa4ok.library.util.Tuple;
 
 import java.io.IOException;
+import java.util.List;
 
 //https://coderlessons.com/tutorials/java-tekhnologii/izuchite-apache-http-client/apache-httpclient-kratkoe-rukovodstvo
 
@@ -33,23 +36,39 @@ public class HttpNetworkManager
         }
     }
 
-    public HttpPost createJsonPost(String url, String json)
+    public HttpPost createJsonPost(String url, List<Tuple<String, String>> headers, String json)
     {
         HttpPost post = new HttpPost(url);
         post.setEntity(new StringEntity(json, "UTF-8"));
         post.setHeader("Accept", "application/json");
         post.setHeader("Content-type", "application/json");
         post.setHeader("Accept-Encoding", "UTF-8");
+        if(headers != null && !(headers.isEmpty())) {
+            for(Tuple<String, String> t : headers) {
+                post.setHeader(t.getFirst(), t.getSecond());
+            }
+        }
         return post;
     }
 
-    public <T> T doJsonPost(String url, String json, Class<T> responseClass) throws Exception
+    public <T extends JsonServerMessage> T doJsonPost(String url, List<Tuple<String, String>> headers, String json, Class<T> responseClass) throws Exception
     {
-        CloseableHttpResponse response = httpClient.execute(createJsonPost(url, json));
+        CloseableHttpResponse response = httpClient.execute(createJsonPost(url, headers, json));
         if(response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
             throw new HttpException(response.getStatusLine().getStatusCode() + " " + response.getStatusLine().getReasonPhrase());
         }
         String jsonResponse = EntityUtils.toString(response.getEntity(), "UTF-8");
+
+        JsonServerMessage serverMessage = GsonUtil.gson.fromJson(jsonResponse, JsonServerMessage.class);
+        if(serverMessage.error != null) {
+            throw   new HttpException("Server error: " + serverMessage.error);
+        }
+
         return GsonUtil.gson.fromJson(jsonResponse, responseClass);
+    }
+
+    public <T extends JsonServerMessage> T doJsonPost(String url, String json, Class<T> responseClass) throws Exception
+    {
+        return doJsonPost(url, null, json, responseClass);
     }
 }
