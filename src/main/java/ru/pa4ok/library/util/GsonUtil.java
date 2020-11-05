94160fd3cc8b620ffc3e938953af1dd9a6cb42e3
Pa4ok <n_leontiev@mail.ru>
2020-11-05 04:05:31 +0300

    update GsonUtil, add Exclude field anotation

--- src/main/java/ru/pa4ok/library/util/GsonUtil.java
+++ src/main/java/ru/pa4ok/library/util/GsonUtil.java
@@ -1,22 +1,41 @@
 package ru.pa4ok.library.util;
 
+import com.google.gson.ExclusionStrategy;
+import com.google.gson.FieldAttributes;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 public class GsonUtil
 {
-	private static final GsonBuilder gsonBuilder;  
+	private static final GsonBuilder gsonBuilder;
+
 	public static final Gson gson;
 	public static final Gson gsonPretty;
-	
+
 	static {
 		gsonBuilder = new GsonBuilder();
-		gsonBuilder.serializeNulls();  
-		
+		gsonBuilder.serializeNulls();
+		gsonBuilder.setExclusionStrategies(new ExclusionStrategy() {
+			@Override
+			public boolean shouldSkipClass(Class<?> clazz) {
+				return false;
+			}
+
+			@Override
+			public boolean shouldSkipField(FieldAttributes field) {
+				return field.getAnnotation(Exclude.class) != null;
+			}
+		});
+
 		gson = gsonBuilder.create();
 		gsonPretty = gsonBuilder.setPrettyPrinting().create();
 	}
-	
+
 	public static class Jsonable
 	{
 		@Override
@@ -24,10 +43,14 @@ public class GsonUtil
 		{
 			return gson.toJson(this);
 		}
-		
+
 		public String toPrettyString()
 		{
 			return gsonPretty.toJson(this);
 		}
 	}
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target(ElementType.FIELD)
+	public static @interface Exclude {}
 }
