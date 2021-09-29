226e9d1792decde13433903be340b0456ff6c7f8
Pa4ok <n_leontiev@mail.ru>
2021-09-29 13:18:13 +0300

    update HttpNetworkManager

--- src/main/java/ru/pa4ok/library/network/HttpNetworkManager.java
+++ src/main/java/ru/pa4ok/library/network/HttpNetworkManager.java
@@ -1,4 +1,4 @@
-package ru.pa4ok.library.network.http;
+package ru.pa4ok.library.network;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -24,14 +24,18 @@ import java.util.List;
  * some tips
  * https://coderlessons.com/tutorials/java-tekhnologii/izuchite-apache-http-client/apache-httpclient-kratkoe-rukovodstvo
  */
-public class HttpNetworkManager implements AutoCloseable {
+public class HttpNetworkManager implements AutoCloseable
+{
     private static final Logger logger = LogManager.getLogger(HttpNetworkManager.class);
+
+    public static final boolean LOG_LONG_BODY = false;
 
     private final Gson gson = GsonUtil.gson;
     private final CloseableHttpClient httpClient;
 
-    public HttpNetworkManager(RequestConfig requestConfig) {
-        if (requestConfig != null) {
+    public HttpNetworkManager(RequestConfig requestConfig)
+    {
+        if(requestConfig != null) {
             this.httpClient = HttpClientBuilder.create().setDefaultRequestConfig(requestConfig).build();
         } else {
             this.httpClient = HttpClients.createDefault();
@@ -47,7 +51,8 @@ public class HttpNetworkManager implements AutoCloseable {
         this.httpClient.close();
     }
 
-    private HttpPost createJsonPost(String url, List<Tuple<String, String>> headers, Object toJsonEntity) {
+    private HttpPost createJsonPost(String url, List<Tuple<String, String>> headers, Object toJsonEntity)
+    {
         HttpPost post = new HttpPost(url);
         if (toJsonEntity != null) {
             post.setEntity(new StringEntity(gson.toJson(toJsonEntity), "UTF-8"));
@@ -61,21 +66,22 @@ public class HttpNetworkManager implements AutoCloseable {
         return post;
     }
 
-    public <V, T> T doJsonPost(String url, List<Tuple<String, String>> headers, V clientMessage, Class<T> responseClass) throws HttpException {
+    public <V, T> T doJsonPost(String url, List<Tuple<String, String>> headers, V clientMessage, Class<T> responseClass) throws HttpException
+    {
         try {
-            logger.debug("Send post request: url='" + url + "'" + (headers != null ? ", headers='" + headers.toString() : "") + "'" + ", body='" + gson.toJson(clientMessage) + "'");
+            logger.debug("Send post request: url='" + url + "'" + (headers != null ? " headers='" + headers + "'": "") + " body='" + checkBody(gson.toJson(clientMessage)) + "'");
+            long mills = System.currentTimeMillis();
             CloseableHttpResponse response = httpClient.execute(createJsonPost(url, headers, clientMessage));
             if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
                 throw new HttpException(response.getStatusLine().getStatusCode() + (response.getEntity() == null ? "" : " " + EntityUtils.toString(response.getEntity(), "UTF-8")));
             }
-            if (responseClass == null) {
-                return null;
-            }
-            if (response.getEntity() == null) {
+            mills = System.currentTimeMillis() - mills;
+            if (responseClass == null || response.getEntity() == null) {
+                logger.debug("Get post response: url='" + url + "' time='" + mills + "ms' body='null'");
                 return null;
             }
             String jsonResponse = EntityUtils.toString(response.getEntity(), "UTF-8");
-            logger.debug("Get post response: " + jsonResponse);
+            logger.debug("Get post response: url='" + url + "' time='" + mills + "ms' body='" + checkBody(jsonResponse) + "'");
 
             return gson.fromJson(jsonResponse, responseClass);
         } catch (IOException e) {
@@ -83,23 +89,35 @@ public class HttpNetworkManager implements AutoCloseable {
         }
     }
 
-    public <V, T> List<T> doJsonPostForList(String url, List<Tuple<String, String>> headers, V clientMessage, Class<T> responseClass) throws HttpException {
+    public <V, T> List<T> doJsonPostForList(String url, List<Tuple<String, String>> headers, V clientMessage, Class<T> responseClass) throws HttpException
+    {
         try {
-            logger.debug("Send post request: url='" + url + "'" + (headers != null ? ", headers='" + headers.toString() : "") + "'" + ", body='" + gson.toJson(clientMessage) + "'");
+            logger.debug("Send post request: url='" + url + "'" + (headers != null ? " headers='" + headers + "'": "") + " body='" + checkBody(gson.toJson(clientMessage)) + "'");
+            long mills = System.currentTimeMillis();
             CloseableHttpResponse response = httpClient.execute(createJsonPost(url, headers, clientMessage));
             if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
                 throw new HttpException(response.getStatusLine().getStatusCode() + (response.getEntity() == null ? "" : " " + EntityUtils.toString(response.getEntity(), "UTF-8")));
             }
+            mills = System.currentTimeMillis() - mills;
             if (response.getEntity() == null) {
+                logger.debug("Get post response: url='" + url + "' time='" + mills + "ms' body='null'");
                 return null;
             }
             String jsonResponse = EntityUtils.toString(response.getEntity(), "UTF-8");
-            logger.debug("Get post response: " + jsonResponse);
+            logger.debug("Get post response: url='" + url  + "' time='" + mills + "ms' body='" + checkBody(jsonResponse) + "'");
 
             return gson.fromJson(jsonResponse, TypeToken.getParameterized(List.class, responseClass).getType());
         } catch (IOException e) {
             throw new HttpException("IO error in request", e);
         }
+    }
+
+    private String checkBody(String body)
+    {
+        if(body != null && body.length() > 512) {
+            return "[LONG_BODY_HIDDEN]";
+        }
+        return body;
     }
 
     public CloseableHttpClient getHttpClient() {
