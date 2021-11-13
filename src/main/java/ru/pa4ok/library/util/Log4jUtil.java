aa5ee10c8c882d1312fa9e280c729cfc4cfa200c
Pa4ok <n_leontiev@mail.ru>
2021-11-14 01:36:50 +0300

    add path to log4j util

--- src/main/java/ru/pa4ok/library/util/Log4jUtil.java
+++ src/main/java/ru/pa4ok/library/util/Log4jUtil.java
@@ -2,17 +2,12 @@ package ru.pa4ok.library.util;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.core.config.Configurator;
-import org.apache.logging.log4j.core.config.builder.api.AppenderComponentBuilder;
-import org.apache.logging.log4j.core.config.builder.api.ComponentBuilder;
-import org.apache.logging.log4j.core.config.builder.api.ConfigurationBuilder;
-import org.apache.logging.log4j.core.config.builder.api.ConfigurationBuilderFactory;
-import org.apache.logging.log4j.core.config.builder.api.LayoutComponentBuilder;
-import org.apache.logging.log4j.core.config.builder.api.RootLoggerComponentBuilder;
+import org.apache.logging.log4j.core.config.builder.api.*;
 import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration;
 
 public class Log4jUtil
 {
-    public static void configureLogging(String logName)
+    public static void configureLogging(String path, String logName)
     {
         ConfigurationBuilder<BuiltConfiguration> builder = ConfigurationBuilderFactory.newConfigurationBuilder();
 
@@ -23,7 +18,7 @@ public class Log4jUtil
         builder.add(rootLogger);
 
         ComponentBuilder policy = builder.newComponent("Policies");
-        policy.addComponent(builder.newComponent("CronTriggeringPolicy").addAttribute("schedule", "0 0 0 * * ?"));
+        policy.addComponent(builder.newComponent("TimeBasedTriggeringPolicy").addAttribute("interval", "1").addAttribute("modulate", true));
         policy.addComponent(builder.newComponent("SizeBasedTriggeringPolicy").addAttribute("size", "100M"));
 
         LayoutComponentBuilder layout = builder.newLayout("PatternLayout");
@@ -39,8 +34,8 @@ public class Log4jUtil
         builder.add(file);*/
 
         AppenderComponentBuilder rolling = builder.newAppender("rolling", "RollingFile");
-        rolling.addAttribute("fileName", "logs/" + logName + ".log");
-        rolling.addAttribute("filePattern", "logs/%d{dd-MM-yy}-" + logName + ".log.gz");
+        rolling.addAttribute("fileName", path + "logs/" + logName + ".log");
+        rolling.addAttribute("filePattern", path + "logs/%d{dd-MM-yy}-" + logName + ".log.gz");
         rolling.addComponent(policy);
         rolling.add(layout);
         builder.add(rolling);
@@ -49,6 +44,6 @@ public class Log4jUtil
     }
 
     public static void configureLogging() {
-        configureLogging("application");
+        configureLogging("", "application");
     }
 }
