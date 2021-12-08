3bcac80e0026319f400cd48360445d0775240ab2
Pa4ok <n_leontiev@mail.ru>
2021-12-08 11:58:23 +0300

    update tuple using lombok

--- src/main/java/ru/pa4ok/library/util/Tuple.java
+++ src/main/java/ru/pa4ok/library/util/Tuple.java
@@ -1,36 +1,14 @@
 package ru.pa4ok.library.util;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Tuple<S,V>
 {
     private S first;
     private V second;
-
-    public Tuple(S first, V second) {
-        this.first = first;
-        this.second = second;
-    }
-
-    @Override
-    public String toString() {
-        return "Tuple{" +
-                "first=" + first +
-                ", second=" + second +
-                '}';
-    }
-
-    public S getFirst() {
-        return first;
-    }
-
-    public void setFirst(S first) {
-        this.first = first;
-    }
-
-    public V getSecond() {
-        return second;
-    }
-
-    public void setSecond(V second) {
-        this.second = second;
-    }
 }
