01e7a06c9fa58f2e86dcd27cff6ab33f70b8cca7
Pa4ok <n_leontiev@mail.ru>
2022-10-26 23:26:47 +0300

    update convert service

--- src/main/java/ru/pa4ok/library/spring/hibernate/ConvertService.java
+++ src/main/java/ru/pa4ok/library/spring/hibernate/ConvertService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 /**
  * T - main (server)
@@ -15,34 +16,46 @@ public interface ConvertService<T,K>
 {
     DefaultService<T> getService();
 
-    Function<T,K> getConvertFunction();
+    Function<T,K> convert();
+
+    default Function<K,T> restoreOnAdd() {
+        throw new UnsupportedOperationException("this function need to be overridden for use add(k) & addAll(k[])");
+    }
+
+    default Function<K,T> restoreOnEdit() {
+        throw new UnsupportedOperationException("this function need to be overridden for use edit(k) & editAll(k[]) & delete(k) & deleteAll(k[])");
+    }
 
     @Transactional(readOnly = true)
     default Optional<K> findById(long id) {
-        return getService().findById(id).map(getConvertFunction());
+        return getService().findById(id).map(convert());
     }
 
     @Transactional(readOnly = true)
     default List<K> findAll() {
         return getService().findAll().stream()
-                .map(getConvertFunction())
+                .map(convert())
                 .collect(Collectors.toList());
     }
 
     default T add(K entity) {
-        throw new UnsupportedOperationException();
+        return getService().add(restoreOnAdd().apply(entity));
     }
 
     default List<T> addAll(Iterable<K> entities) {
-        throw new UnsupportedOperationException();
+        return getService().addAll(StreamSupport.stream(entities.spliterator(), false)
+                .map(restoreOnAdd())
+                .collect(Collectors.toList()));
     }
 
     default T edit(K entity) {
-        throw new UnsupportedOperationException();
+        return getService().edit(restoreOnEdit().apply(entity));
     }
 
     default List<T> editAll(Iterable<K> entities) {
-        throw new UnsupportedOperationException();
+        return getService().editAll(StreamSupport.stream(entities.spliterator(), false)
+                .map(restoreOnEdit())
+                .collect(Collectors.toList()));
     }
 
     default T deleteById(long id) {
@@ -50,7 +63,7 @@ public interface ConvertService<T,K>
     }
 
     default void delete(K entity) {
-        throw new UnsupportedOperationException();
+        getService().delete(restoreOnEdit().apply(entity));
     }
 
     default void deleteAll() {
@@ -58,6 +71,8 @@ public interface ConvertService<T,K>
     }
 
     default void deleteAll(Iterable<K> entities) {
-        throw new UnsupportedOperationException();
+        getService().deleteAll(StreamSupport.stream(entities.spliterator(), false)
+                .map(restoreOnEdit())
+                .collect(Collectors.toList()));
     }
 }
